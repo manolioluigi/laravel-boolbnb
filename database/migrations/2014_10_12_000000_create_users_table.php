88b4c232f4466f16731647a9e5e177cdfeb28b64
Reagonfly <reagon@hotmail.it>
2023-03-22 17:29:43 +0100

    done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->date('birth_date')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
