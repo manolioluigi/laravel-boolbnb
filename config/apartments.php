f0462b1f8923c562c6176493d0a84c384d10b5ed
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-03-23 11:58:59 +0100

    2 apartaments

--- config/apartments.php
+++ config/apartments.php
@@ -2,17 +2,31 @@
     return $apartment =
     [
         [
-            'title' => "",
-            'description' => "",
-            'room_n' => "",
-            'bed_n' => "",
-            'bath_n' => "",
-            'square_meters' => "",
+            'title' => "Charm & Tradition Japan-Yui Valley",
+            'description' => "Una sosta rigenerante tra Tokyo e Kyoto. In campagna, semplice casa contadina tradizionale circondata da montagne verdi lussureggianti, foreste di bambù, fiumi e campi da tè. Fuori dai soliti percorsi turistici, scopri il vero lato rurale del Giappone.",
+            'room_n' => "6",
+            'bed_n' => "2",
+            'bath_n' => "2",
+            'square_meters' => "100",
             'visible' => true,
-            'address' => "",
+            'address' => "Okuizumo, Nita District, Prefettura di Shimane, Giappone",
             'latitude' => "",
             'longitude' => "",
-            'cover_image' => "",
+            'cover_image' => "https://a0.muscache.com/im/pictures/40222820/44f9ed81_original.jpg?im_w=1200",
+        ],
+        [
+            'title' => "Nurdachhaus Marie nella Foresta Nera",
+            'description' => "Rendi la tua vacanza un'esperienza molto speciale e indimenticabile in un Nurdachhaus nella Foresta Nera.
+            Una regione con diversi paesaggi naturali e una vasta gamma di opportunità culturali, di intrattenimento e di svago, ma anche un'economia dinamica. Il Nurdachhaus si trova nel bel mezzo di un parco vacanze ai margini della città d'oro di Pforzheim e può offrire sia l'azione e l'avventura, nonché il relax e il relax grazie alla sua ottima posizione.",
+            'room_n' => "7",
+            'bed_n' => "2",
+            'bath_n' => "2",
+            'square_meters' => "80",
+            'visible' => true,
+            'address' => "Neuhausen, Baden-Württemberg, Germania",
+            'latitude' => "",
+            'longitude' => "",
+            'cover_image' => "https://a0.muscache.com/im/pictures/miso/Hosting-50503787/original/15230787-28ca-4405-a89d-968f324d4c93.jpeg?im_w=1200",
         ],
     ]
 
