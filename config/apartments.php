94fedde102417461ac303c1bb16f36647e351737
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-03-23 12:01:13 +0100

    end array apartments

--- config/apartments.php
+++ config/apartments.php
@@ -14,6 +14,32 @@
             'longitude' => "",
             'cover_image' => "",
         ],
+        [
+            'title' => "La Stalla - Casa San Gabriel",
+            'description' => "Situata sulle colline che si affacciano su una splendida Valle Umbra, con viste ininterrotte fino ad Assisi, Casa San Gabriel è una collezione di 4 casali risalenti al XVI secolo. Amorevolmente restaurate dagli attuali proprietari, le case dispongono di travi originali, piastrelle di terracotta e le ultime tecnologie che danno il massimo comfort.",
+            'room_n' => "3",
+            'bed_n' => "1",
+            'bath_n' => "1",
+            'square_meters' => "100",
+            'visible' => true,
+            'address' => "Pierantonio, Umbria, Italia",
+            'latitude' => "",
+            'longitude' => "",
+            'cover_image' => "https://a0.muscache.com/im/pictures/37672380/503142ce_original.jpg?im_w=1440",
+        ],
+        [
+            'title' => "La Calcara. Spiaggia privata",
+            'description' => "Villa panoramica in  tenuta di ettari  vista panoramica sulla baia di Amalfi.  La pulizia a brillo e la disinfezione di tutte le superfici sono il nostro forte, controllare le recensioni precedenti. Massima sicurezza da contagi  con spiaggia privata, grande piscina marina e consegna di cibo. Decorazioni interne in stile mediterraneo.  Adatta ai bambini, privacy, ampi terreni. 6 camere da letto 7 bagni, parcheggio per 5 veicoli. Su richiesta cucina, per cena, pranzo e colazione",
+            'room_n' => "14",
+            'bed_n' => "6",
+            'bath_n' => "7",
+            'square_meters' => "250",
+            'visible' => true,
+            'address' => "Amalfi coast , Campania, Italia",
+            'latitude' => "",
+            'longitude' => "",
+            'cover_image' => "https://a0.muscache.com/im/pictures/miso/Hosting-4971155/original/983adc0c-ce92-4138-9d23-81f5abe2d12e.jpeg?im_w=1200",
+        ],
     ]
 
 ?>
