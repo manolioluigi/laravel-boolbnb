b737e302c6c89cba443fd598d276c2bf3145e713
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-23 16:59:03 +0100

    index.blade completo

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -9,7 +9,7 @@
                         <h1>ELENCO POST</h1>
                     </div>
                     <div>
-                        <a class="btn btn-primary" href="{{route('admin.posts.create')}}">Aggiungi Post</a>
+                        <a class="btn btn-primary" href="{{route('admin.apartments.create')}}">Aggiungi Post</a>
                     </div>
                 </div>
             </div>
@@ -29,22 +29,22 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @forelse ($posts as $post)
+                        @forelse ($apartments as $apartment)
                         <tr>
-                            <th scope="row">{{ $post->id }}</th>
-                            <td>{{ $post->title }}</td>
-                            <td>{{ $post->slug }}</td>
+                            <th scope="row">{{ $apartment->id }}</th>
+                            <td>{{ $apartment->title }}</td>
+                            <td>{{ $apartment->slug }}</td>
                             <td>
-                                <a href="{{route('admin.posts.show', $post->slug)}}" title="Visualizza post" class="btn btn-sm btn-square btn-primary">
+                                <a href="{{route('admin.apartments.show', $apartment->slug)}}" title="Visualizza apartment" class="btn btn-sm btn-square btn-primary">
                                     <i class="fas fa-eye"></i>
                                 </a>
-                                <a href="{{route('admin.posts.edit', $post->slug)}}" title="Modfica post" class="btn btn-sm btn-square btn-warning">
+                                <a href="{{route('admin.apartments.edit', $apartment->slug)}}" title="Modfica apartment" class="btn btn-sm btn-square btn-warning">
                                     <i class="fas fa-edit"></i>
                                 </a>
                                 <button type="submit" class="btn btn-sm btn-square btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                     <i class="fas fa-trash"></i>
                                 </button>
-                                {{-- <form class="d-inline-block" action="{{route('admin.posts.destroy', $post->slug)}}" method="POST">
+                                {{-- <form class="d-inline-block" action="{{route('admin.apartments.destroy', $apartment->slug)}}" method="POST">
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-sm btn-square btn-danger">
@@ -56,7 +56,7 @@
                         @empty
                         <tr>
                             <td scope="row">
-                                Nessun post, aggiungilo da <a href="{{route('admin.posts.create')}}">qui</a> 
+                                Nessun post, aggiungilo da <a href="{{route('admin.apartments.create')}}">qui</a> 
                             </td>
                         </tr>        
                         @endforelse
@@ -65,6 +65,5 @@
             </div>
         </div>
     </div>
-    @include('admin.partials.modals')
 
 @endsection
