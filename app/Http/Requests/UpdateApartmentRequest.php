76904866f99febcfc1166388492bfdbbe0a77405
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-03-24 13:10:48 +0100

    update

--- app/Http/Requests/UpdateApartmentRequest.php
+++ app/Http/Requests/UpdateApartmentRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class UpdateApartmentRequest extends FormRequest
 {
@@ -25,14 +24,14 @@ class UpdateApartmentRequest extends FormRequest
     public function rules()
     {
         return [           
-            'title'         => ['required', Rule::unique('apartments')->ignore($this->apartment), 'string', 'max:255'],
-            'description'   => ['nullable'],
-            'room_n'        => ['integer', 4, 'required'],
-            'bed_n'         => ['integer', 4, 'required'],
-            'bath_n'        => ['integer', 4, 'required'],
-            'square_meters' => ['integer', 11, 'required'],
-            'address'       => ['string', 'max:255', 'required'],
-            'cover_image'   => ['string', 'required'],
+            'title'         => 'required|string|max:255',
+            'description'   => 'nullable',
+            'room_n'        => 'integer|max:4| required',
+            'bed_n'         => 'integer|max:4| required',
+            'bath_n'        => 'integer|max:4| required',
+            'square_meters' => 'integer|max:11| required',
+            'address'       => 'string|max:255|required',
+            'cover_image'   => 'string|required'
         ];
     }
 
