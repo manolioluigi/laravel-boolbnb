0cb34ad874f9070ee1afafb0bf5a5abefb8d974f
Alexbarile <alexbarile93fg@yahoo.it>
2023-03-30 19:31:23 +0200

    problema cors

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -15,7 +15,7 @@ class TrustHosts extends Middleware
     {
         return [
             $this->allSubdomainsOfApplicationUrl(),
-            'http://127.0.0.1:8000/guest/search'
+           
             
         ];
     }
