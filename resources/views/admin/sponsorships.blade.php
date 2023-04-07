31c270fe4e6741734998ae7eadf05276fdef2a44
Alexbarile <alexbarile93fg@yahoo.it>
2023-04-07 18:38:21 +0200

    payment

--- resources/views/admin/sponsorships.blade.php
+++ resources/views/admin/sponsorships.blade.php
@@ -28,50 +28,51 @@
     
     <div class="container">
         <div class="row">
-            <div class="col-12 d-flex justify-content-center my-5">
-              @if (session('success_message'))
-                <div class="alert alert-success">
-                    {{ session('success_message') }}
-                </div>
-              @endif
-              @if (count($errors) > 0 )
-                <div class="alert alert-danger">
-                  <ul>
-                    @foreach ($errors as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                  </ul>
-                </div>
-              @endif
-              <div class="card shadow-lg w-50 p-5">
-
-                <img class="card-img-top w-50 align-self-center" src="/img/BOOLBNB-removebg-ok.png" alt="logo_bnb">
-
-                <form method="post" id="payment-form" action="{{ url('/admin/checkout') }}">
-                  @csrf
-                    <section class="mt-3">
-                        <input type="hidden" name="apartment_id" value="@php $apartment_id = request()->input('id'); echo $apartment_id; @endphp">
-                        <input type="hidden" name="price" value="@php $price_id = request()->input('price'); echo $price_id; @endphp">
-                        <label for="amount">
-                            <span class="input-label">Amount</span>
-                            <div class="input-wrapper amount-wrapper">
-                                <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="@php $price = request()->input('price'); echo $price; @endphp" disabled>
-                            </div>
-                        </label>
-    
-                        <div class="bt-drop-in-wrapper my-5">
-                            <div id="bt-dropin"></div>
-                        </div>
-                    </section>
-    
-                    <input id="nonce" name="payment_method_nonce" type="hidden" />
-                    <div class="d-flex justify-content-between">
-                      <button class="btn btn-primary" type="submit"><span>Pay Now</span></button>
-                      <a href="{{ route('admin.sponsorships.index') }}" class="btn btn-secondary">Back to List</a>
-                    </div>
-                </form>
+          <div class="col-12 d-flex justify-content-center mt-5 w-100">
+            @if (session('success_message'))
+              <div class="alert alert-success w-50">
+                  {{ session('success_message') }}
               </div>
+            @endif
+            @if (count($errors) > 0 )
+              <div class="alert alert-danger w-50">
+                <ul>
+                  @foreach ($errors as $error)
+                      <li>{{$error}}</li>
+                  @endforeach
+                </ul>
+              </div>
+            @endif
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 d-flex justify-content-center mt-3">
+            <div class="card shadow-lg w-50 p-5">
+              <img class="card-img-top w-50 align-self-center" src="/img/BOOLBNB-removebg-ok.png" alt="logo_bnb">
+              <form method="post" id="payment-form" action="{{ url('/admin/checkout') }}">
+                @csrf
+                  <section class="mt-3">
+                      <input type="hidden" name="apartment_id" value="@php $apartment_id = request()->input('id'); echo $apartment_id; @endphp">
+                      <input type="hidden" name="price" value="@php $price_id = request()->input('price'); echo $price_id; @endphp">
+                      <label for="amount">
+                          <span class="input-label">Amount</span>
+                          <div class="input-wrapper amount-wrapper">
+                              <input id="amount" name="amount" type="tel" min="1" placeholder="Amount" value="@php $price = request()->input('price'); echo $price; @endphp" disabled>
+                          </div>
+                      </label>
+
+                      <div class="bt-drop-in-wrapper my-5">
+                          <div id="bt-dropin"></div>
+                      </div>
+                  </section>
+                  <input id="nonce" name="payment_method_nonce" type="hidden" />
+                  <div class="d-flex justify-content-between">
+                    <button class="btn btn-primary" type="submit"><span>Pay Now</span></button>
+                    <a href="{{ route('admin.sponsorships.index') }}" class="btn btn-secondary">Back to List</a>
+                  </div>
+              </form>
             </div>
+          </div>
         </div>
     </div>
 
