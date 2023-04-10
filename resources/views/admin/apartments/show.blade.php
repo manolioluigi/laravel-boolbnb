cbfdde57ad51f5adfe72f389c4f0044dbaee2c40
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-04-10 16:04:10 +0200

    update show.blade

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -20,21 +20,23 @@
                 <img src="{{ asset('storage/' .$apartment->cover_img) }}" alt="{{$apartment->title}}" class="img-fluid img-show cards-img">
             </div>
             <hr class="my-5">
-            <p class="d-block"><i class="me-2 fas fa-house-user"></i><strong>Description:</strong> {{ $apartment->description }}</p>
-            <p><i class="me-2 fas fa-door-closed"></i><strong>Local n.: </strong>{{ $apartment->room_n }}</p>
-            <p><i class="me-2 fas fa-bed"></i><strong>Rooms n.: </strong>{{ $apartment->bed_n }}</p>
-            <p><i class="me-2 fas fa-shower"></i><strong>Bathrooms: </strong>{{ $apartment->bath_n }}</p>
-            <p><i class="me-2 fas fa-house-chimney"></i><strong>Square Meters: </strong>{{ $apartment->square_meters }}</p>
-            <p><i class="me-2 fab fa-periscope"></i><strong>Address: </strong>{{ $apartment->address }}</p>
-            <p><i class="me-2 fas fa-star"></i><strong>Sponsorship: </strong>
+            <p class="d-block"><i class="me-2 fas fa-house-user"></i><strong>Description:</strong> <span class="text-secondary">{{ $apartment->description }}</span></p>
+            <p><i class="me-2 fas fa-door-closed"></i><strong>Local n.: </strong> <span class="text-secondary">{{ $apartment->room_n }}</span></p>
+            <p><i class="me-2 fas fa-bed"></i><strong>Rooms n.: </strong><span class="text-secondary">{{ $apartment->bed_n }}</span></p>
+            <p><i class="me-2 fas fa-shower"></i><strong>Bathrooms: </strong><span class="text-secondary">{{ $apartment->bath_n }}</span></p>
+            <p><i class="me-2 fas fa-house-chimney"></i><strong>Square Meters: </strong><span class="text-secondary">{{ $apartment->square_meters }}</span></p>
+            <p><i class="me-2 fab fa-periscope"></i><strong>Address: </strong><span class="text-secondary">{{ $apartment->address }}</span></p>
+            <p><i class="me-2 fas fa-star"></i><strong>Sponsorship: </strong><span class="text-secondary">
                 @foreach ($apartment->sponsorships as $sponsorship)
                 {{$sponsorship['name']}}
                 @endforeach
+            </span>
             </p>
-            <p><i class="me-2 fas fa-list"></i><strong>Optionals: </strong>
+            <p><i class="me-2 fas fa-list"></i><strong>Optionals: </strong><span class="text-secondary">
                 @foreach ($apartment->optionals as $optional)
                 {{$optional['name']}},
                 @endforeach
+            </span>
             </p>
         </div>
         <hr class="mt-5">
